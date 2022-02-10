d0c752dc03da751d20f21eec4937c24ee27a7832
Gaurav Nelson <ganelson@outlook.com>
2022-02-10 20:31:36 +1000

    Added chrome extension

---
+++ Chrome/icons/openmodule.js
@@ -0,0 +1,13 @@
+var links = document.links;
+for (var i = 0; i < links.length; i++) {
+  if (
+    links[i].href.match(
+      /https?:\/\/(www\.)?github.com\/openshift\/openshift-docs\/blob\/+.*\/modules\//
+    )
+  ) {
+    console.info("Updating the following original link to point to the module");
+    console.log("%c" + links[i].href, "color: blue");
+    links[i].style.textDecoration = "underline green 2px";
+    links[i].href = links[i].href.replace(/(?<=\/blob\/.\w+\/).+\/(?=modules)/, "");
+  }
+}
